8d1e618e316b2392c6b56684d76be701f743091c
dili000 <dilqem9645409@gmail.com>
2022-08-24 05:22:39 -0700

    test 3 newbranch

---
+++ src/main/kotlin/Eolymp/Problems.kt
@@ -0,0 +1,103 @@
+package Eolymp
+
+import java.math.RoundingMode
+import java.util.Arrays
+import java.util.Scanner
+import kotlin.math.min
+import kotlin.math.round
+import kotlin.math.roundToInt
+
+fun main() {
+
+//      P: 5059
+//     val scanner =Scanner(System.`in`)
+//    var n = scanner.nextInt()
+//    var arrayInt = Array(n){0}
+//    var min1=0
+//    var max=0
+//    var min2=0
+//    for(i in arrayInt.indices) {
+//       arrayInt[i] = scanner.nextInt()
+//        min1= arrayInt.min()
+//        max= arrayInt.max()
+//
+//    }
+//    println(min1)
+//    for(i in arrayInt.indices) {
+//        if (arrayInt[i]==min1)
+//        {
+//            arrayInt[i]=max
+//        }
+//
+//    }
+//    min2=arrayInt.min()
+//    println(Arrays.toString(arrayOf(min1,min2)))
+
+
+
+
+//      P: 1952
+//     val scanner =Scanner(System.`in`)
+//    var n = scanner.nextInt()
+//    var arrayInt = Array(n){0}
+//    var min=0
+//    var max=0
+//    for(i in arrayInt.indices) {
+//       arrayInt[i] = scanner.nextInt()
+//        min= arrayInt.min()
+//        max= arrayInt.max()
+//        println(min)
+//
+//    }
+//    for(i in arrayInt.indices) {
+//
+//
+//        if (arrayInt[i]==max)
+//        {
+//            arrayInt[i]=min
+//        }
+//
+//    }
+//    println(Arrays.toString(arrayInt))
+
+
+//    P:  7365
+//         val scanner =Scanner(System.`in`)
+//    var arrayDouble = DoubleArray(scanner.nextInt())
+//    var say=0
+//    var sud=0.0
+//    for(i in arrayDouble.indices) {
+//        arrayDouble[i] = scanner.nextDouble()
+//
+//if (arrayDouble[i]<30.0){
+//
+//    say++
+//
+//}
+//        sud=say.toDouble()
+//sud=sud*200/900
+//
+// sud= Math.ceil(sud)
+//        println(sud)
+//    }
+//
+//    print(" tort= "+say+" süd= "+sud.toInt())
+
+//    P:  931
+//         val scanner =Scanner(System.`in`)
+//
+//    val m = scanner.nextLong().toString().toCharArray()
+//    var n="0"
+//    var a= 1
+//    var b=0
+//    var c= 0.0
+//    for (i in m.indices) {
+//        n = m[i].toString();
+//        a *= Integer.parseInt(n)
+//        b += Integer.parseInt(n)
+//    }
+//    println(a)
+//    println(b)
+//    c= (a.toDouble()/b.toDouble())
+//    print(c.toBigDecimal().setScale(3, RoundingMode.UP))
+}
--- src/main/kotlin/Eolymp/Pronlem1.kt
+++
@@ -1,8 +0,0 @@
-package Eolymp
-
-class Pronlem1 {
-
-
-
-
-}
